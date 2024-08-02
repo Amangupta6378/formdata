9479143802e4e3430a1604e3ca922ecf5293e1a6
vikas <shahv7411@gmail.com>
2024-08-02 14:32:29 +0530

    formdata

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+    isVisible: true,
+    mode: "online-mode"
+  });
+
+  // console.log(formData.firstName, formData.lastName, formData.email, formData.comments)
+  console.log(formData);
+
+  function changeHandler(event) {
+    const { name, checked, value, type } = event.target;
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value
+      };
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Enter your first name"
+        onChange={changeHandler}
+        name="firstName"
+        value={formData.firstName}
+      />
+      <br />
+      <br />
+
+      <input
+        type="text"
+        placeholder="Enter your last name"
+        onChange={changeHandler}
+        name="lastName"
+        value={formData.lastName}
+      />
+      <br />
+      <br />
+
+      <input
+        type="email"
+        placeholder="Enter your email"
+        onChange={changeHandler}
+        name="email"
+        value={formData.email}
+      />
+      <br />
+      <br />
+
+      <textarea
+        type="text"
+        placeholder="Enter your comments"
+        onChange={changeHandler}
+        name="comments"
+        value={formData.comments}
+      />
+      <br />
+      <br />
+
+      <input
+        type="checkbox"
+        onChange={changeHandler}
+        name="isVisible"
+        id="isVisible"
+        checked={formData.isVisible}
+      />
+      <label htmlFor="isVisible">Am I Visible</label>
+      <br />
+      <br />
+
+      <input
+        type="radio"
+        onChange={changeHandler}
+        name="mode"
+        value="online-mode"
+        id="online-mode"
+        checked={formData.mode === "online-mode"}
+      />
+      <label htmlFor="online-mode">Online mode</label>
+      <br />
+      <br />
+
+      <input
+        type="radio"
+        onChange={changeHandler}
+        name="mode"
+        value="offline-mode"
+        id="offline-mode"
+        checked={formData.mode === "offline-mode"}
+      />
+      <label htmlFor="offline-mode">Offline mode</label>
     </div>
   );
 }
